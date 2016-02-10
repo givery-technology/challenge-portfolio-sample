58211bda7326f70270a94e4295d54157b8036502
sukeshni <sukeshni.kantrod@gmail.com>
2016-02-10 12:23:33 +0900

    Mod database.sql for postgresql

--- specifications/database.sql
+++ specifications/database.sql
@@ -1,9 +1,9 @@
 -- This file is formatted based on SQLite3's syntax
 -- Some small fixes might be needed to use in other database.
-create table projects (
-    id integer PRIMARY KEY AUTOINCREMENT,
+ create table projects (
+    id serial PRIMARY KEY,
     url varchar(255) NULL,
     title varchar(255) NOT NULL,
     description text NOT NULL,
-    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at date NOT NULL DEFAULT CURRENT_DATE
   );
